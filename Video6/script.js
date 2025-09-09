a72739c609836bc0d2af3334fc265149c88f731a
SudhanshuBaberwal <24bcs147@iiitdwd.ac.in>
2025-09-09 14:16:58 +0530

    Video6

---
+++ Video6/script.js
@@ -0,0 +1,36 @@
+function breakText() {
+  var h1 = document.querySelector("h1");
+  var h1Text = h1.textContent;
+
+  var splittedText = h1Text.split("");
+  var halfValue = Math.floor(splittedText.length / 2);
+
+  var clutter = "";
+
+  splittedText.forEach((elem, idx) => {
+    if (idx < halfValue) {
+      clutter += `<span class="a">${elem}</span>`;
+    } else {
+      clutter += `<span class="b">${elem}</span>`;
+    }
+  });
+
+  h1.innerHTML = clutter;
+}
+
+breakText();
+
+gsap.from(".a", {
+  y: 70,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.5,
+  stagger: 0.2,
+});
+gsap.from(".b", {
+  y: 70,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.5,
+  stagger: -0.2,
+});
